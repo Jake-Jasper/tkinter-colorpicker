6bb0f43f13e63ef2aa27845969f1bbd253276a41
Jake Jasper <35571955+Jake-Jasper@users.noreply.github.com>
2021-03-27 11:25:49 +0000

    Update example.py

--- example.py
+++ example.py
@@ -6,19 +6,22 @@ class color_picker:
     def rgbtohex(self, r,g,b):
         return f'#{r:02x}{g:02x}{b:02x}'
 
-
     def update_col(self, val):
         self.display_box.configure(bg = self.rgbtohex(self.R_scale.get(),self.G_scale.get(),self.B_scale.get()))
 
-    def color_chooser(self):
-        window = tk.Tk()
-        window.geometry("200x200")
+    def close_dialog(self):
+        print(self.rgbtohex(self.R_scale.get(),self.G_scale.get(),self.B_scale.get()))
+        self.window.destroy()
 
-        self.R_scale = Scale(master = window, orient='horizontal', from_=0, to=255)
+    def color_chooser(self):
+        self.window = tk.Tk()
+        self.window.geometry("200x200")
+
+        self.R_scale = Scale(master = self.window, orient='horizontal', from_=0, to=255)
         self.R_scale.grid(row = 0, column = 0)
-        self.G_scale = Scale(master = window, orient='horizontal', from_=0, to=255)
+        self.G_scale = Scale(master = self.window, orient='horizontal', from_=0, to=255)
         self.G_scale.grid(row = 1, column = 0)
-        self.B_scale = Scale(master = window, orient='horizontal', from_=0, to=255)
+        self.B_scale = Scale(master = self.window, orient='horizontal', from_=0, to=255)
         self.B_scale.grid(row = 2, column = 0)
 
         self.R_scale.bind("<ButtonRelease-1>", self.update_col)
@@ -26,12 +29,11 @@ class color_picker:
         self.B_scale.bind("<ButtonRelease-1>", self.update_col)
 
 
-        self.display_box = Canvas(master = window, width =90, height = 125, bg = self.rgbtohex(self.R_scale.get(),self.G_scale.get(),self.B_scale.get()))
+        self.display_box = Canvas(master = self.window, width =90, height = 125, bg = self.rgbtohex(self.R_scale.get(),self.G_scale.get(),self.B_scale.get()))
         self.display_box.grid(row = 0, rowspan =3, column = 1)
 
-
-        self.Ok_button = Button(master = window, text = 'Okay', command = None).grid(row = 3, column = 0)
-        self.close_button = Button(master = window, text = 'Close', command = window.destroy).grid(row = 3, column = 1)
+        self.Ok_button = Button(master = self.window, text = 'Okay', command = self.close_dialog).grid(row = 3, column = 0)
+        self.close_button = Button(master = self.window, text = 'Close', command = self.window.destroy).grid(row = 3, column = 1)
 
 
 def main():
